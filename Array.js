690157d80f67a891f289cc79ce19fd46ef63fd0e
Abhinav-bM <abhinavbmanoj@gmail.com>
2023-10-31 19:45:24 +0530

    checked-boolean-return

--- Array.js
+++ Array.js
@@ -1,3 +1,12 @@
-const a = [[10,15,20],[[35,660,500],[100,200,300]]]
+// const a = [[10,15,20],[[35,660,500],[100,200,300]]]
 
-console.log(a[1][0][1]);
+// console.log(a[1][0][1]);
+
+
+// returning boolean from functions
+
+function booleanreturn(a, b){
+    return  a < b ;
+}
+
+console.log(booleanreturn(10, 2));
