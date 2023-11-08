79adc2683c89b478e99fc46641a71b009e61932b
Abhinav-bM <abhinavbmanoj@gmail.com>
2023-11-08 20:02:26 +0530

    update

---
+++ again.js
@@ -0,0 +1,32 @@
+// let x = 5;
+// let y = 6;
+
+// let z=x+y;
+
+// console.log(z);
+
+
+// const a = "hello world, ";
+
+// const b = "Lets Study Js"
+
+// const c = a+b;
+
+// console.log(c);
+
+// const price1 = 5;
+
+// const price2 = 6;
+
+// price2 = 10;  // error-Assignment to constant variable
+
+// console.log(price2);
+
+// let a = 5, b = 6, c = 8;  // declared in asingle line
+ 
+// let z = a+b+c;
+
+// console.log(z);
+
+
+
