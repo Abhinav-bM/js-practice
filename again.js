fbb298dc2c5a7151c27d550974d821ec966a32df
Abhinav-bM <abhinavbmanoj@gmail.com>
2023-11-09 18:31:21 +0530

    update

--- again.js
+++ again.js
@@ -30,3 +30,127 @@
 
 
 
+
+// this will show error - x has already been declared
+// let x = 5;
+
+// let x = 4;
+
+// console.log(x);
+
+// using var the second declaration will over ride the first
+// var x = 54;
+// var x = 5;
+
+// console.log(x);
+
+
+
+// block scope // can't have to acces variable declared using let or const inside a {}
+// {
+//     const y = 10;
+//     let x = 10;
+//     var z =10;
+// }
+
+// console.log(z);
+
+//redeclare variable with let, in another block is allowed
+
+// let x = 10;
+
+// {
+//     let x = 15;
+// }
+// console.log(x);
+
+// reassigning
+
+
+// let x = 10;  // with let you can reassign
+//     x = 12;
+
+//     console.log(x);
+
+// const y = 5;  // you can't have to reassign a variable which is declared by cosnt keyword
+//     y = 5;
+
+//     console.log(y);
+
+// const x; // you have to  assign a value when delclaring with const
+// x = 5;
+
+// console.log(x);
+
+// const x = [1,2,3,4,5,6]
+
+// x = [2,4,7,8] // cant't have to reassign a array which is declared by const
+
+// // but you can change the an element, and you can add element
+
+// console.log(x);
+
+// object with const
+    // const cars = {
+    //     brand : "bmw",
+    //     color : "red"
+    // }
+
+    // cars = {             // can't have to redeclare a object which is declred by const
+    //     brand : "benz",
+    //     color : "yellow"
+    // }
+
+// you can add or change the object property
+
+//  const cars = {
+//     brand : "bmw",
+//     color : "red"
+// }
+
+// cars.brand = "audi"; // can change the property
+
+
+// cars.owner = "john"; // can add new property
+
+// console.log(cars);
+
+// boolean
+
+// const x = 5;
+
+// const y = 4;
+
+// console.log((x==y));
+
+
+// Array
+
+// const myArray = [1,2,3,3,4,45,5];
+
+// const myString = ["heyy", "hello", "howareyou"]
+
+// console.log(myString);
+
+// object
+
+// const myCar = {
+//     brand : "Honda",
+//     color : "Red",
+//     model : "Civic"
+// }
+
+// myCar.year = 2012;
+
+// console.log(myCar);
+
+
+
+            
+
+
+
+
+
+
+
