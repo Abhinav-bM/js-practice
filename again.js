0ec0eb38ddeb72f54f77f02ada376cc19d724a0c
Abhinav-bM <abhinavbmanoj@gmail.com>
2023-11-11 07:38:35 +0530

    update

--- again.js
+++ again.js
@@ -263,7 +263,7 @@ console.log(day);*/
 
 // for loop
 
-const a = [1,2,3,4,5,6,7,8,9]
+// const a = [1,2,3,4,5,6,7,8,9]
 
 // for (i = 0; i < a.length; i++){
 //     console.log(a[i]);
@@ -350,4 +350,219 @@ const a = [1,2,3,4,5,6,7,8,9]
 //     i++;
 // }
 
+// functions-----------------------
+// function myFunction(a, b){
+//     return a * b;
+// }
 
+// console.log(myFunction(10,10));
+// function used as variable
+
+// function check (num1, num2){
+//     return (num1 + num2);
+// }
+
+// let x = check(12,3);
+
+// console.log(x);
+
+//local variable----variable insid a function become local scope
+// function localCheck(b){
+//     let num1 = 10;
+
+//     return num1+b;
+// }
+
+// console.log(num1); // it will show an error num1 is not defined
+
+//functions can also be defined as expressions
+// let x = function (a, b){return a + b};
+
+// console.log(x(1,2)); // after stored function in a variable, the variable can used as function
+
+//Functions can also be defined with a built-in JavaScript function constructor called Function().
+// const myFunction = new Function("a", "b", "return a + b");
+// let x = myFunction(10, 5);
+
+// console.log(x);
+
+
+//self invoke functions
+// (function (){
+//     console.log("hello");
+// })();
+
+
+
+
+//function are objects because they have properties and methods
+// argument.length is a property
+// function check (a, b){
+//     return arguments.length;  
+// }
+// console.log(check(1, 2));
+
+// toString()--------------
+// function check(a, b){
+//     return a + b;
+// }
+
+// console.log(check.toString());
+
+// ----------Arrow Functions ------------
+// const x = (a, b) => a + b;
+
+// console.log(x(1, 2));
+
+//Sequence control------
+
+// function myCalculator(a,b){
+//     return a + b;
+// }
+
+// function outPut(result){
+//     console.log(result);
+// }
+
+// let x = myCalculator(10, 5);
+
+// outPut(x);
+
+
+// Call back function
+
+//examples:
+// function myFunction(some){
+//     console.log(some);
+// }
+
+// function calcSum(num1, num2, func){
+//     let sum = num1 + num2;
+//     func(sum);
+// }
+
+// calcSum(1, 2, myFunction)
+
+// const myNumbers = [4, 1, -20, -7, 5, 9, -6];
+
+// const positiveNum = removeNeg(myNumbers, (x) => x >= 0);
+
+// console.log(positiveNum);
+
+// function removeNeg(numbers, callback){
+//     let newArray = [];
+//     for(let x of numbers){
+//         if(callback(x)){
+//             newArray.push(x);
+//         }
+//     }
+//     return newArray;
+// }
+
+
+//-------Arrays----------
+// const cars = new Array("bmw", "Audi", "Honda", "Mercedez", "volvo", "Porsche") // you can create array like this 
+
+// const cars = ["bmw", "Audi", "Honda", "Mercedez", "volvo", "Porsche"] // also can create like this---use this mehthod always
+
+
+// for (let x of cars){
+//     console.log(x);
+// }
+// const cars = ["bmw", "Audi", "Honda", "Mercedez", "volvo", "Porsche"]
+
+// can convert array to string using toString(), method.
+
+// const cars = ["bmw", "Audi", "Honda", "Mercedez", "volvo", "Porsche"]
+// console.log(cars.toString()); // this will out the array as string, separated by commas
+
+// const cars = ["bmw", "Audi", "Honda", "Mercedez", "volvo", "Porsche"]
+// console.log(cars); // can access full array element
+
+// Array-- methods-----------
+
+//Length
+// const cars  = ["bmw", "Audi", "mercedez", "Honds"];
+// console.log(cars.length);
+
+// toString()
+// const bikes = ["honda", "Yamaha", "Ducati", "Aprilia"];
+// console.log(bikes.toString());
+
+// join()
+// const fruits = ["Apple", "Orange", "Mango"];
+// console.log(fruits.join(" * "));
+
+// pop()
+// const vegetables = ["tomato", "beans", "Onion"];    
+// console.log(vegetables.pop());
+
+// push()
+// const cars = ["model", "brand", "color"];
+// cars.push("year");
+// console.log(cars);
+
+// shift()
+// const mern = ["mongoDB", "ExpressJS", "React", "Node"];
+// mern.shift();
+// console.log(mern);
+
+// unShift()
+// const cars = ["bmw", "Audi", "Porsche"];
+// cars.unshift("mercedez");
+// console.log(cars);
+
+//delete
+// const cars = ["bmw", "Audi", "Porsche"];
+// delete cars[0];
+// console.log(cars);
+
+// concat()
+// const mern = ["mongoDB", "ExpressJS", "React", "Node"]
+// const carBrands = ["bmw", "Audi", "Porsche"];
+// const carModels = ["5Series", "911", "RS7"];
+// const carsCombine = mern.concat(carBrands, carModels);
+// console.log(carsCombine);
+
+// flat()
+// const numbers = [[1,2], [3,4], [5,6,7,8,9]];
+// const newArray = numbers.flat();
+// console.log(newArray);
+
+// splice()
+// const fruits = ["Banana", "Orange", "Apple"]
+// fruits.splice(2,0, "Lemon", "Kiwi")
+// console.log(fruits);
+
+// slice()
+// const fruits = ["Banana", "Orange", "Apple", "banana"]
+// // const citrus = fruits.slice(1)
+// const citrus = fruits.slice(1,3);
+// console.log(citrus);
+
+
+// multi dimentional array
+// const myarray = [
+//     [1,2,3,4,5,6,[1,3,4]],
+//     [7,8,9,10],
+//     [,11,12,13,14]
+// ]
+// console.log(myarray[0][6][2]);
+
+
+//------------Objects-------------
+// const myObj = {
+//     firstName : "John",
+//     secondName : "Doe",
+//     Age : 25
+// }
+
+
+const myObj = {
+    firstName : "Abhinav",
+    secondName : "B manoj",
+    age : 22,
+    fullName : function(){return this.firstName+ " "+this.secondName}
+}
+
+console.log(myObj.fullName());
--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,5 +1,1 @@
-const check = [1,2,3,4,5,6,7]
-
-for (let x in check){
-    console.log(check[x]);
-}
+const a = [1,2,3,4,5,6,7,8,9]
