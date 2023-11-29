f682b1f8fea7f540ab2248f0dbb728c1c2ec7629
Abhinav-bM <abhinavbmanoj@gmail.com>
2023-11-29 12:17:08 +0530

    strigMethod

---
+++ stringMethods.js
@@ -0,0 +1,52 @@
+let myStr = "ABhinav"
+
+// Length
+console.log(myStr.length);
+
+// toUpperCase toLowerCase
+let upper = myStr.toUpperCase()
+let lower = myStr.toLowerCase()
+console.log(upper)
+
+// Trim
+let myStr1 = "    abhinav   "
+let trimmed = myStr1.trim()
+let trimStart = myStr1.trimStart()
+let trimEnd = myStr1.trimEnd()
+console.log(trimEnd);
+
+// pad 
+let myStr2 = "Abhi"
+let padstart = myStr2.padStart(10, 0)
+let padEnd = myStr2.padEnd(7, "nav")
+console.log(padEnd);
+
+// charAt
+let mystr3 = "abhinav"
+console.log(mystr3.charAt(6));
+
+// split
+let myStr4 = "abhin-av"
+let splitted = myStr4.split('')
+console.log(splitted);
+
+// concat
+let str1 = "Abhinav "
+let str2 = "B Manoj"
+let concated = str1.concat(str2)
+console.log(concated);
+
+// join
+let myArr = ['a', 'b', 'h', 'i']
+let joined = myArr.join(',')
+console.log(joined);
+
+// slice
+let myArray6 = 'abhinav'
+let sliced = myArray6.slice(0,4)
+console.log(sliced);
+
+// substring
+let myArray7 = 'abhinav'
+let substring = myArray7.substring(0,4)
+console.log(substring);
