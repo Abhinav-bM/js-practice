588ede249d79f7d26eb9e9ae8da5f1d174768a87
Abhinav-bM <abhinavbmanoj@gmail.com>
2024-01-05 19:11:33 +0530

    update

--- map,filter,reduce.js
+++ map,filter,reduce.js
@@ -35,14 +35,8 @@
 // You are given the length and width of a 4-sided polygon. The polygon can either be a rectangle or a square.
 //If it is a square, return its area. If it is a rectangle, return its perimeter
 
-function areaOrperimeter (l,m){
-    if(l === m){
-        return l*m;
-    }
-    else{
-        return 2 * (l + m)
-    }
-}
+
+
 
 
 console.log(areaOrperimeter(6,10));
