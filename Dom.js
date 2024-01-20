ebca4898b56d9c14991458a76839afd74f465ae0
Abhinav-bM <abhinavbmanoj@gmail.com>
2024-01-20 20:09:24 +0530

    update

--- Dom.js
+++ Dom.js
@@ -1,24 +1,83 @@
 
-// var x = 7;
+// // var x = 7;
 
-// function greet (){
-//     console.log('hello world');
-// }
+// // function greet (){
+// //     console.log('hello world');
+// // }
 
-// greet()
-// console.log(x);
-// console.log(greet);
+// // greet()
+// // console.log(x);
+// // console.log(greet);
 
-let myArr = [1,2,2,2,2,2,23,3,3]
+// // let myArr = [1,2,2,2,2,2,23,3,3]
 
-let uniqueSet = new Set(myArr)
-let newArr = []
+// // let uniqueSet = new Set(myArr)
+// // let newArr = []
 
-// for(let x of uniqueSet){
-//     newArr.push(x)
-// }
+// // // for(let x of uniqueSet){
+// // //     newArr.push(x)
+// // // }
 
-uniqueSet.forEach((x)=>newArr.push(x))
+// // uniqueSet.forEach((x)=>newArr.push(x))
 
 
-console.log(newArr)
+// // console.log(newArr)
+
+// // let a = 10 
+
+// // console.log(a);
+// // greet()
+
+// // function greet (){
+// //     console.log("Heloooooooooooooooooooooo")
+// // }
+
+
+
+
+// let array = [
+    
+//     {
+//         home:{
+//             siblings:"3",
+//             members:2,
+//             address:[{city:"kochi"},{pin:111111}]
+//         },
+//         school:"svghsss"
+//     },
+
+//     {
+//         home:{
+//             siblings:"0",
+//             members:6,
+//             address:[{city:"malappuram"},{pin:222222}]
+//         },
+//         school:"gvhss"
+//     },
+
+//     {
+//         home:{
+//             siblings:"2",
+//             members:7,
+//             address:[{city:"kozhikode"},{pin:333333}]
+//         },
+//         school:"scss"
+//     },
+// ]
+
+// array.forEach((x)=>{
+//     x.home.address.forEach((y)=>{
+        
+//         if(y.city === 'kozhikode'){
+//             y.city = "Kottyam"
+//         }
+//         console.log(y);
+//     })
+// })
+
+
+
+// let a = require('./export')
+// a.muti(1,2)
+
+
---
+++ export.js
@@ -0,0 +1,11 @@
+// let a = 'llqq'
+// let newArr = []
+
+// for(i=a.length-1; i>=0; i--){
+//     newArr.push(a[i])
+// }
+
+//  console.log(newArr.join(''));
+
+
+
