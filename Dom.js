e453046a9b1cd4e2128d9ce63001752dd2f61185
Abhinav-bM <abhinavbmanoj@gmail.com>
2024-01-20 09:26:20 +0530

    update

--- Dom.js
+++ Dom.js
@@ -8,3 +8,17 @@
 // greet()
 // console.log(x);
 // console.log(greet);
+
+let myArr = [1,2,2,2,2,2,23,3,3]
+
+let uniqueSet = new Set(myArr)
+let newArr = []
+
+// for(let x of uniqueSet){
+//     newArr.push(x)
+// }
+
+uniqueSet.forEach((x)=>newArr.push(x))
+
+
+console.log(newArr)
