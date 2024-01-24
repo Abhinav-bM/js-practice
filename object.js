c165540d3f8891cfd42e53fc1ca4d896360a2dc1
Abhinav-bM <abhinavbmanoj@gmail.com>
2024-01-24 14:03:36 +0530

    update

--- object.js
+++ object.js
@@ -10,17 +10,17 @@
 //     },10000)
 // })
 
-function myPromise(x) {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      if (x === 0) {
-        resolve("promise resolved");
-      } else {
-        reject("promise rejected");
-      }
-    }, 3000);
-  });
-}
+// function myPromise(x) {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       if (x === 0) {
+//         resolve("promise resolved");
+//       } else {
+//         reject("promise rejected");
+//       }
+//     }, 3000);
+//   });
+// }
 
 // async function divide() {
 //   try {
@@ -39,7 +39,6 @@ function myPromise(x) {
 // }
 // divide();
 
-
 // myPromise(0)
 // .then((data)=>{
 //     console.log(data);
@@ -48,14 +47,77 @@ function myPromise(x) {
 //     console.log(err);
 // })
 
-const promise1 = Promise.resolve('Promise 1 resolved');
-const promise2 = Promise.resolve('Promise 2 resolved');
-const promise3 = Promise.resolve('Promise 3 resolved');
+// const promise1 = Promise.resolve('Promise 1 resolved');
+// const promise2 = Promise.resolve('Promise 2 resolved');
+// const promise3 = Promise.resolve('Promise 3 resolved');
 
-Promise.all([promise1, promise2, promise3])
-  .then((results) => {
-    console.log('All promises resolved:', results);
+// Promise.all([promise1, promise2, promise3])
+//   .then((results) => {
+//     console.log('All promises resolved:', results);
+//   })
+//   .catch((error) => {
+//     console.error('One or more promises were rejected:', error);
+//   })
+
+// const shoppingCart = [
+//   { name: "Laptop", price: 1000 },
+//   { name: "MobilePhone", price: 500 },
+//   {
+//     name: "Furniture",
+//     price: 200,
+//     items: [
+//       { name: "Chair", price: 50 },
+//       { name: "Table", price: 150 },
+//     ],
+//   },
+//   { name: "Headphones", price: 80 },
+// ];
+
+// function calculateTotal(arr) {
+//   let sum1 = 0;
+//   let sum2 = 0;
+//   arr.forEach((x) => {
+//     if (x.items) {
+//       x.items.forEach((y) => {
+//         sum2 += y.price;
+//       });
+//     } else {
+//       sum1 += x.price;
+//     }
+//   });
+
+//   return sum1 + sum2;
+// }
+// console.log(calculateTotal(shoppingCart));
+
+// let str = "abhinav"
+
+// console.log(str[1]);
+
+const nestedObject = {
+  array1: [
+    { name: "ObjectA", value: 10 },
+    { name: "ObjectB", value: 20 },
+    { name: "ObjectC", value: 30 },
+  ],
+  array2: [
+    { title: "Item1", quantity: 5 },
+    { title: "Item2", quantity: 10 },
+    { title: "Item3", quantity: 15 },
+  ],
+  array3: [
+    { firstName: "John", lastName: "Doe", age: 25 },
+    { firstName: "Jane", lastName: "Smith", age: 30 },
+    { firstName: "Bob", lastName: "Johnson", age: 22 },
+  ],
+};
+
+for(let key in nestedObject){
+  nestedObject[key].forEach((elem)=>{
+    for(let x in elem){
+      if(elem[x] === 'Jane'){
+        elem[x] = "Hamim"
+      }
+    }
   })
-  .catch((error) => {
-    console.error('One or more promises were rejected:', error);
-  })
+}
