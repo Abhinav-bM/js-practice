b54cd3460ec8f7a865f0016157867dcbf10ffc42
Abhinav-bM <abhinavbmanoj@gmail.com>
2024-02-01 08:58:12 +0530

    update

--- DSA.js
+++ DSA.js
@@ -1,18 +1,22 @@
-let a = [6,5,4,3,9,8,0]
+// let a = [6,5,4,3,9,8,0]
 
-function result (arr, target){
-    let result = []
-    for(i=0; i<arr.length-1; i++){
-        for(j=1+1; j<arr.length; i++){
-            if(arr[i]+arr[j]===target){
-                result.push(arr[i],arr[j])
-                return result
-            }else{
-                return "not possible"
-            }
-        }
-    }
-}
+// function result (arr, target){
+//     let result = []
+//     for(i=0; i<arr.length-1; i++){
+//         for(j=1+1; j<arr.length; i++){
+//             if(arr[i]+arr[j]===target){
+//                 result.push(arr[i],arr[j])
+//                 return result
+//             }else{
+//                 return "not possible"
+//             }
+//         }
+//     }
+// }
 
 
-console.log(result(a,10));
+// console.log(result(a,10));
+
+
+// move a number to the last of the array
+let a = [6,1,6,8,10,4,15,6,,3,9,6]
