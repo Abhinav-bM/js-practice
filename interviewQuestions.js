15f56513f33ec9ec27f86148e2f3c2db26c292b9
Abhinav-bM <abhinavbmanoj@gmail.com>
2024-08-20 21:59:09 +0530

    update

---
+++ interviewQuestions.js
@@ -0,0 +1,18 @@
+function sumOfTwo(a,b){
+    return a+b
+}
+
+
+// Write a JavaScript program to find the maximum number in an array. 
+
+function maxOfAnArray(arr){
+    let max = 0
+    for(let i=0; i<arr.length; i++){
+        if(arr[i] > max){
+            max = arr[i]
+        }
+    }
+    return max
+}
+
+console.log(maxOfAnArray([1,2,3,4,15]))
